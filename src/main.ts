9fbdf393234b3f351969344452305313e28962fb
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-07-10 21:27:27 +0900

    Update code with linter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import Vue from "vue";
-import './plugins/vuetify'
+import { CreateElement, VNode } from "vue";
+import "./plugins/vuetify";
 import App from "./App.vue";
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h: CreateElement): VNode => h(App)
 }).$mount("#app");
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
-import ja from 'vuetify/src/locale/ja'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
+import ja from "vuetify/src/locale/ja";
 
 Vue.use(Vuetify, {
   options: {
     customProperties: true
   },
-  iconfont: 'md',
+  iconfont: "md",
   lang: {
     locales: { ja },
-    current: 'ja'
-  },
-})
+    current: "ja"
+  }
+});
