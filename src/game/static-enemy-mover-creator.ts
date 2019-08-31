31dc928ee4406a93fadf3fa9a7b416e6c7d6bbf7
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-08-31 23:28:42 +0900

    Update StticEnemyMoverCreator

--- src/game/static-enemy-mover-creator.ts
+++ src/game/static-enemy-mover-creator.ts
@@ -1,24 +1,68 @@
 import { StaticEnemyMover } from "./static-enemy-mover";
+import { EnemyMoveRoute } from "./enemy-move-route";
+import { StraightMoveRoute } from "./contents/enemy-move-route/straight-move-route";
+import { EventDispatcher } from "./event-dispatcher";
 
-export interface StaticEnemyMoverCreatorArgs {
-  // TODO: Add route enum
-  activateTime: number;
-  moveSpeedInArea: number;
-  moveAngleDegInArea: number;
+/**
+ * Enemy move route types.
+ */
+export enum EnemyMoveRouteType {
+  sideMove = "sideMove"
+  // TODO: Add new types later
 }
 
+export interface StaticEnemyMoverCreatorArgs {
+  routeType: EnemyMoveRouteType;
+  activateTime: number;
+  moveSpeedInArea: number;
+  isLeftSide: boolean;
+}
+
+/**
+ * Create StaticEnemyMover with route.
+ * Route was specified by `routeType` in arguments.
+ */
 export class StaticEnemyMoverCreator {
+  private readonly routeType: EnemyMoveRouteType;
   private readonly activateTime: number;
   private readonly moveSpeedInArea: number;
-  private readonly moveAngleDegInArea: number;
+  private readonly isLeftSide: boolean;
 
   public constructor(args: StaticEnemyMoverCreatorArgs) {
+    this.routeType = args.routeType;
     this.activateTime = args.activateTime;
     this.moveSpeedInArea = args.moveSpeedInArea;
-    this.moveAngleDegInArea = args.moveAngleDegInArea;
+    this.isLeftSide = args.isLeftSide;
   }
 
-  public create(_activePosInArea: ex.Vector): StaticEnemyMover {
-    throw new Error("Implement this."); // TODO: Implement this
+  /**
+   * Create new StaticEnemyMover.
+   *
+   * @param activePosInArea Route activate position.
+   */
+  public create(activePosInArea: ex.Vector): StaticEnemyMover {
+    return new StaticEnemyMover({
+      route: this.createRoute(activePosInArea),
+      onEnteringToArea: new EventDispatcher<void>(),
+      onExitingFromArea: new EventDispatcher<void>()
+    });
+  }
+
+  private createRoute(activePosInArea: ex.Vector): EnemyMoveRoute {
+    if (this.routeType === EnemyMoveRouteType.sideMove) {
+      return this.createSideMoveRoute(activePosInArea);
+    }
+    // TODO: Add new types later
+    throw new Error("Unknown route type");
+  }
+
+  private createSideMoveRoute(activePosInArea: ex.Vector): EnemyMoveRoute {
+    const moveAngleDegInArea = this.isLeftSide ? -10 : -170;
+    return new StraightMoveRoute({
+      activePosInArea,
+      moveAngleDegInArea,
+      activateTime: this.activateTime,
+      moveSpeedInArea: this.moveSpeedInArea
+    });
   }
 }
