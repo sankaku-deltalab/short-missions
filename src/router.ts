a0ff6ca7964786e494edc4c71d59adb1b2725659
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-09-05 12:37:39 +0900

    Fix router type

--- src/router.ts
+++ src/router.ts
@@ -19,7 +19,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: (): Promise<typeof import("*.vue")> =>
+      component: (): any =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
