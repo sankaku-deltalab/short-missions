39754291b378fb44dd1f82af3feb8653b4d665bf
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-09-16 22:29:43 +0900

    Add TopEnter

---
+++ src/game/contents/activate-position-generator/top-enter.ts
@@ -0,0 +1,50 @@
+import * as ex from "excalibur";
+import { ActivatePositionGenerator } from "@/game/enemies-builder/activate-position-generator";
+
+export interface ActivateTimeAndPosition {
+  timeSec: number;
+  position: ex.Vector;
+}
+
+/**
+ * Entering enemies from top of left or right.
+ */
+export class SideEnter implements ActivatePositionGenerator {
+  public generate(
+    spawnNum: number,
+    enemyKillTimeSec: number,
+    enemySizeInArea: ex.Vector,
+    squadKillTimeSec: number,
+    isLeftSide: boolean
+  ): ActivateTimeAndPosition[] {
+    const horizontalNumMax = Math.max(
+      1,
+      Math.floor(0.9 * (0.5 / enemySizeInArea.y))
+    );
+    const horizontalNum = Math.min(spawnNum, horizontalNumMax);
+    const verticalNumMax = Math.ceil(spawnNum / horizontalNum);
+    const spawnDuration =
+      spawnNum > 1 ? (squadKillTimeSec - enemyKillTimeSec) / (spawnNum - 1) : 0;
+    const ySign = isLeftSide ? -1 : 1;
+    const posXMin = 0.2;
+    const posXDiff = 0.2 / verticalNumMax;
+    const posYMin = Math.min(0.25, 0.25 / horizontalNum);
+    const posYDiff = 0.5 / horizontalNum;
+    return Array(spawnNum)
+      .fill(0)
+      .map(
+        (_: number, count: number): ActivateTimeAndPosition => {
+          const verticalIdx = Math.floor(count / horizontalNum);
+          const horizontalIdx = count - verticalIdx * horizontalNum;
+          const position = new ex.Vector(
+            posXMin + verticalIdx * posXDiff,
+            ySign * (posYMin + horizontalIdx * posYDiff)
+          );
+          return {
+            timeSec: spawnDuration * count,
+            position
+          };
+        }
+      );
+  }
+}
